65723336b95638b5eb201bdb3c3f7fdfc35154cf
Chantale444 <nzeggechantal@yahoo.com>
2025-09-18 21:22:45 +0200

    Commits done
    Just did the last commit. Added screen shots to the READ ME file

---
+++ pages/About.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+# Title
+st.title("About - Chantale XYZ")
+
+# Summary (Markdown formatted)
+st.markdown("""
+### About this Project  
+
+This is a brief summary of my data science project during the DSHI course.  
+The course deepened my interest in machine learning and data science, and I had to burn the midnight oil!  
+
+I conducted exploratory data analysis on the UCI Income dataset, downloaded from Kaggle.  
+The dataset included features such as age, education level, and marital status. My main question was:  
+_Is an individual's income correlated to these demographic factors?_  
+
+I performed data preprocessing, visualization, and correlation analysis.  
+Later, I explored predictive modeling to classify income levels using algorithms like K-Nearest Neighbors and Random Forest.  
+
+The course concluded with a storytelling exercise, which was both challenging and rewarding.  
+This project made me even more motivated to continue learning in the field of data science.
+""")
